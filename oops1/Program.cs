eeaa33b37c0234ef6c223c3ee18c5a18fc60aec4
Abdul <srehaman18@gmail.com>
2024-06-06 10:36:32 +0530

    Add project files.

---
+++ oops1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops1
+{
+    class abdul
+    {
+        int a;
+        internal void xtra()
+        {
+            int x=10;
+            Console.WriteLine(x);
+            Console.WriteLine("gud");
+        }
+       
+    }
+    class rehaman
+    {
+        
+    }
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            abdul obj = new abdul();
+            obj.xtra();
+        }
+    }
+}
